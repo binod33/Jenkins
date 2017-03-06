2a0f508926c8a006e150a1c480a0e0ca80a26be9
Binod KC <binod.kc@concordusa.com>
2017-03-06 12:24:00 -0600

    Added test

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by binod.kc on 3/6/2017.
+ */
+public class HelloWorldTest {
+    @Test
+    public void sampleTest(){
+        assertTrue(true);
+    }
+}
