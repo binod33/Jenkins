74f62e5f0fdd37c80fefcd1562b8e76a508ee949
Binod KC <binod.kc@concordusa.com>
2017-03-06 10:01:56 -0600

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by binod.kc on 3/6/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+    }
+}
---
+++ src/test/java/HelloWorldTest.groovy
@@ -0,0 +1,12 @@
+import org.junit.Test
+
+/**
+ * Created by binod.kc on 3/6/2017.
+ */
+class HelloWorldTest extends GroovyTestCase {
+
+    @Test
+    public void test(){
+        assert(true);
+    }
+}
