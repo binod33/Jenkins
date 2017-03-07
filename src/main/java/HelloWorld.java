0c7e5b22cb67fb32056b85e424eb1fa8d3e40a0d
Binod KC <binod.kc@concordusa.com>
2017-03-07 11:45:35 -0600

    Clean up

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,6 @@ import java.util.regex.Matcher;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is a test for new new branch new ");
+        System.out.println("This is a test for a clean addition.");
     }
 }
